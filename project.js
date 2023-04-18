a848db9fc1021dffabced446f5b96fd787f93054
Lavkush Kumar <78034400+Lavkush22@users.noreply.github.com>
2023-04-18 18:24:50 +0530

    project page

---
+++ project.js
@@ -0,0 +1,82 @@
+let projects = [
+    {
+        name: "project one",
+        tags: "#javascript",
+        image: "js1.png",
+    },
+    {
+        name: "project two",
+        tags: "#javascript",
+        image: "js1.png",
+    },
+    {
+        name: "project three",
+        tags: "#javascript",
+        image: "js1.png",
+    },
+    {
+        name: "project four",
+        tags: "#javascript",
+        image: "js1.png",
+    },
+    {
+        name: "project one",
+        tags: "#fullstackj",
+        image: "f1.png",
+    },
+    {
+        name: "project two",
+        tags: "#fullstackj",
+        image: "f1.png",
+    },
+    {
+        name: "project three",
+        tags: "#fullstackj",
+        image: "f1.png",
+    },
+    {
+        name: "project four",
+        tags: "#fullstackj",
+        image: "f1.png",
+    },
+    {
+        name: "project one",
+        tags: "#ui/ux",
+        image: "ui1.png",
+    },
+    {
+        name: "project two",
+        tags: "#ui/ux",
+        image: "ui1.png",
+    },
+    {
+        name: "project three",
+        tags: "#ui/ux",
+        image: "ui1.png",
+    },
+    {
+        name: "project four",
+        tags: "#ui/ux",
+        image: "ui1.png",
+    },
+    {
+        name: "project one",
+        tags: "#backend",
+        image: "bk1.png",
+    },
+    {
+        name: "project two",
+        tags: "#backend",
+        image: "bk1.png",
+    },
+    {
+        name: "project three",
+        tags: "#backend",
+        image: "bk1.png",
+    },
+    {
+        name: "project four",
+        tags: "#backend",
+        image: "bk1.png",
+    },
+]
